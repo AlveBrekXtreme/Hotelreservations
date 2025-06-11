4b7e251d9b31691773adfbd3fea89f4922595a3a
Alve <114334244+AlveBrekXtreme@users.noreply.github.com>
2025-06-11 15:20:25 +0200

    Add validations to models<

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :hotel
+
+  validates :first_name, :last_name, :phone, :email, :arrival_date, :depature_date, :number_of_rooms, presence: true
+  validates :phone, length: { is: 8 }
+  validates :arrival_date, comparison: { less_than: :depature_date }
+  validates :number_of_rooms, numericality: { greater_than: 0 }
 end

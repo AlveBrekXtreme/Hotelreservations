830ba67b609f6eb604b3a6833152ff5d992224f0
Alve <114334244+AlveBrekXtreme@users.noreply.github.com>
2025-06-11 14:27:40 +0200

    Create reservations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_11_120512) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_11_121406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,20 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_120512) do
     t.float "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "phone", null: false
+    t.string "email", null: false
+    t.bigint "hotel_id", null: false
+    t.datetime "arrival_date", null: false
+    t.datetime "depature_date", null: false
+    t.integer "number_of_rooms", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hotel_id"], name: "index_reservations_on_hotel_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +49,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_120512) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "reservations", "hotels"
 end

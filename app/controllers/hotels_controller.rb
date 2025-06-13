dcd0a0273c71d06dbd730b50e0cc42966ae9a7c6
Alve <114334244+AlveBrekXtreme@users.noreply.github.com>
2025-06-13 09:35:58 +0200

    Set instance variables from search params

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,7 +1,24 @@
 class HotelsController < ApplicationController
+  before_action :set_search_params, only: :search
+
   def index
   end
 
   def search
+  end
+
+  private
+
+  def search_params
+    params.require(:search)
+            .permit(:city, :arrival_date, :depature_date, :number_of_rooms, :max_price)
+  end
+
+  def set_search_params
+    @city = search_params[:city]
+    @arrival_date = search_params[:arrival_date]
+    @depature_date = search_params[:depature_date]
+    @number_of_rooms = search_params[:number_of_rooms]
+    @max_price = search_params[:max_price]
   end
 end

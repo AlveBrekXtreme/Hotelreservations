be6404fc326cc670927cf7d6ff10d3791858580e
Alve <114334244+AlveBrekXtreme@users.noreply.github.com>
2025-06-13 09:35:10 +0200

    Filter by max price

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,6 +5,8 @@ class Hotel < ApplicationRecord
   validates :name, uniqueness: true
   validates :number_of_rooms, :price, numericality: { greater_than: 0 }
 
+  scope :max_price, ->(price) { where("price <= ?", price) }
+
   def self.available_hotels_in_date_range(reservation_period, number_of_reserved_rooms)
     reserved_rooms_per_hotel = reserved_rooms_per_hotel_in_date_range(reservation_period)
     hotel_ids = available_hotel_ids(reserved_rooms_per_hotel, number_of_reserved_rooms)

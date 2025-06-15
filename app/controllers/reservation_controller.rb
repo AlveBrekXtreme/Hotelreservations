ca4a82286ffe968c97225b8c968193c83fc8458d
Alve <114334244+AlveBrekXtreme@users.noreply.github.com>
2025-06-15 10:35:09 +0200

    Get data that will be prefilled in form

--- app/controllers/reservation_controller.rb
+++ app/controllers/reservation_controller.rb
@@ -1,7 +1,24 @@
 class ReservationController < ApplicationController
+  before_action :prefill_data, only: :new
+
   def new
+    @reservation = Reservation.new(
+      hotel_id: @hotel_id,
+      arrival_date: @arrival_date,
+      depature_date: @depature_date,
+      number_of_rooms: @number_of_rooms
+    )
   end
 
   def create
+  end
+
+  private
+
+  def prefill_data
+    @hotel_id = params[:hotel_id]
+    @arrival_date = params[:arrival_date]
+    @depature_date = params[:depature_date]
+    @number_of_rooms = params[:number_of_rooms]
   end
 end
